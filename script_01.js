240ba9fd48a287f466247f41b2d862021037716f
Raz4core <mail@test.de>
2022-01-19 09:18:29 +0100

    Function 2a

--- script_01.js
+++ script_01.js
@@ -2,3 +2,24 @@
 
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöcken
+// Funktionsaufruf (call)
+//test();
+
+// Funktionsrumpf (body) / callee
+// function test()
+// {
+//     console.log ("Hallo!");
+// }
+
+
+/** Funktionen 02a */
+
+//Aufruf (call)
+ausgabeNamen();
+
+// Function
+function ausgabeNamen() {
+    let firstName = "Anna";
+    console.log ("Lappen!" + firstName);
+}
